c1a05776fe3f637f7c112fe933182f7315e4163b
Veronica Cobos <vcobos178@gmail.com>
2024-10-04 12:24:18 -0500

    04/10

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        MoBurbujaAvanzada mO = new MoBurbujaAvanzada();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el tamanio del arreglo");
+        int tamanio = 0;
+
+        
+        do{
+            System.out.println("Ingrese el tamanio que sea entero positivo");
+            while(!scanner.hasNextInt()){   
+                System.out.println("Ingrese un entero valido");
+                scanner.next();
+            }
+            tamanio = scanner.nextInt();
+            if(tamanio <= 0 ){
+                System.out.println("El tamanio debe ser entero positivo");
+            }
+        }while(tamanio <= 0);
+
+
+        int [] arreglo = new int[tamanio];
+    }
+}
---
+++ src/MoBurbujaAvanzada.java
@@ -0,0 +1,34 @@
+public class MoBurbujaAvanzada {
+    public int[] sortBubbleAvanzado(int[] arreglo){
+        int n = arreglo.length;
+        boolean intercambio;
+        for(int i = 0; i < n; i++){
+            intercambio = false;
+            System.out.println("Pasada " + i);
+            for(int j = 0; j < n - 1 - i; j++){
+                System.out.println("j=" + j + " [j]=" + arreglo[j] + " j+1=" 
+                + (j + 1) + " [j+1]=" + arreglo[j+1]);
+                //Intercambio
+                if(arreglo[j] > arreglo[j + 1]){
+                    System.out.println("Si hay cambio");
+                    int aux = arreglo[j];
+                    arreglo[j] = arreglo[j+1];
+                    arreglo[j+1] = aux;
+                    intercambio = true;
+                } 
+            }
+            if(!intercambio){
+                break;
+            }
+        }
+        //Si no hubo ningun cambio es porque ya esta ordenado 
+        // y no deberia preguntar mas
+        return arreglo;
+    }
+
+    public void printArreglo(int[] arreglo){
+        for(int elemento : arreglo){
+            System.out.print(elemento + " , ");
+        }
+    }
+}
