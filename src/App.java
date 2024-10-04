1124e4a281367ae5c0a5c7156e44207b5b89b924
Veronica Cobos <vcobos178@gmail.com>
2024-10-04 13:03:14 -0500

    04/10

--- src/App.java
+++ src/App.java
@@ -5,28 +5,33 @@ public class App {
         MoBurbujaAvanzada mO = new MoBurbujaAvanzada();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Ingrese el tamanio del arreglo");
-        int tamanio = 0;
+        int tamanio = leerEnteroValido(scanner, "Ingrese el tamanio", false);
 
-        
-        do{
-            System.out.println("Ingrese el tamanio que sea entero positivo");
-            while(!scanner.hasNextInt()){   
-                System.out.println("Ingrese un entero valido");
-                scanner.next();
-            }
-            tamanio = scanner.nextInt();
-            if(tamanio <= 0 ){
-                System.out.println("El tamanio debe ser entero positivo");
-            }
-        }while(tamanio <= 0);
 
         int [] arreglo = new int[tamanio];
         for(int k = 0; k < tamanio; k++ ){
             System.out.println("Ingrese el valor de la posicion " + (k+1));
-            arreglo[k] = scanner.nextInt();
+            arreglo[k] = leerEnteroValido(scanner, "Ingrese el valor de la posicion", true);
         }
-
-        System.out.println(arreglo);
-        
     }
+
+
+    public static int leerEnteroValido(Scanner scanner, String mensaje, boolean permitirNegativo){
+        int numero;
+        do{
+            System.out.println(mensaje);
+            while(!scanner.hasNextInt()){   
+                System.out.println("Ingrese un entero valido");
+                System.out.println(mensaje);
+                scanner.next();
+            }
+            numero = scanner.nextInt();
+            if(!permitirNegativo && numero < 0 ){
+                System.out.println("El tamanio debe ser entero positivo");
+            }
+        }while(!permitirNegativo && numero < 0);
+        return numero;
+    }
+
+
 }
